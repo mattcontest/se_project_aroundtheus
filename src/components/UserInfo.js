1e62e811688555bce2f048136fc15b896b02b719
menth0s <marslite@pm.me>
2024-11-28 15:53:43 +0100

    Cleaned up UserInfo.js

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,8 +5,6 @@ export default class UserInfo {
   }
 
   getUserInfo() {
-    // console.log("This,name", this.name);
-    // console.log("This,job", this.job);
     return {
       name: this.name.textContent,
       description: this.job.textContent,
@@ -14,15 +12,7 @@ export default class UserInfo {
   }
 
   setUserInfo({ profileNameData, profileJobData }) {
-    // console.log("This name", profileNameData);
-    // console.log("This job", profileJobData);
-    // this.name.textContent = profileNameData;
-    // this.job.textContent = profileJobData;
     this.name.textContent = profileNameData;
     this.job.textContent = profileJobData;
-    // this.name.textContent = userInfo.title;
-    // this.job.textContent = userInfo.description;
-    // console.log("Updated name", this.name.textContent);
-    // console.log("Updated job", this.job.textContent);
   }
 }
