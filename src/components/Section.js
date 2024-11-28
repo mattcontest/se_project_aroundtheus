d344ea234dbcd7d0a5bdf238d60a9eb7c6a767aa
menth0s <marslite@pm.me>
2024-11-28 15:52:45 +0100

    Corrected typo in Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,7 @@ export default class Section {
   constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
-    this._containterSelector = document.querySelector(containerSelector);
+    this._containerSelector = document.querySelector(containerSelector);
   }
 
   renderItems(items) {
@@ -12,6 +12,6 @@ export default class Section {
   }
 
   addItem(element) {
-    this._containterSelector.prepend(element);
+    this._containerSelector.prepend(element);
   }
 }
